d570edbacbeb428396c9fd79291518d6ac6b1cf0
unknown <bayuxbca@gmail.com>
2023-03-11 07:44:59 +0700

    create student from laravel RestAPI in VueJS

--- src/views/Students/Create.vue
+++ src/views/Students/Create.vue
@@ -5,6 +5,13 @@
                 <h4>Add Students</h4>
             </div>
             <div class="card-body">
+
+                <ul class="alert alert-warning" v-if="Object.keys(this.errorList).length > 0">
+                    <li class="mb-0 ms-3" v-for="(error, index) in this.errorList" :key="index">
+                        {{ error[0] }}
+                    </li>
+                </ul>
+
                 <div class="mb-3">
                     <label for="">Name</label>
                     <input type="text" class="form-control" v-model="model.student.name">
@@ -21,7 +28,7 @@
                     <label for="">Phone</label>
                     <input type="text" class="form-control" v-model="model.student.phone">
                 </div><div class="mb-3">
-                    <button type="button" class="btn btn-primary">Save</button>
+                    <button type="button" @click="saveStudent" class="btn btn-primary">Save</button>
                 </div>
             </div>
         </div>
@@ -30,10 +37,13 @@
 
 <script>
 
+import axios from 'axios';
+
     export default {
         name: 'studentsCreate',
         data () {
             return {
+                errorList: '',
                 model: {
                     student: {
                         name: '',
@@ -43,6 +53,51 @@
                     }
                     
                 }
+            }
+        },
+        methods: {
+
+            saveStudent() {
+
+                var mythis = this;
+
+                axios.post('http://127.0.0.1:8000/api/students', this.model.student).then( res => {
+
+                    console.log(res.data)
+                    alert(res.data.message);
+
+                    this.model.student = {
+                            name: '',
+                            course: '',
+                            email: '',
+                            phone: '',
+                    }
+
+                    this.errorList = '';
+                })
+                .catch(function (error) {
+
+                    if (error.response) {
+
+                        if(error.response.status == 422) {
+
+                            mythis.errorList = error.response.data.errors;
+
+                        }
+                
+                    // console.log(error.response.data);
+                    // console.log(error.response.status);
+                    // console.log(error.response.headers);
+
+                    } else if (error.request) {
+                    
+                    console.log(error.request);
+                    } else {
+                        
+                    console.log('Error', error.message);
+                    }
+                    })
+
             }
         }
     }
